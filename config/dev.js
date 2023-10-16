5f32cf338d670c7aab2acc8791f76e35ff195beb
geonnyeong oh <geonnyeongoh@geonnyeongs-MacBook-Air.local>
2023-10-17 08:47:52 +0900

    비밀정보보호

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://sparta:aaaa4321@cluster0.mcun02k.mongodb.net/?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 const express =require('express');
 const app = express();
 const port = 5001;
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlendcoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json())
+
+const config=require('./config/key');
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://sparta:aaaa4321@cluster0.mcun02k.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(config.mongoURI
 ).then(()=> console.log('MongoDB Connected!'))
     .catch(err => console.log(err))
 
-app.get('/', (req,res)=> res.send('Hello World!!'))
+app.get('/', (req,res)=> res.send('Hello World!! yey'))
 
 app.listen(port,() => console.log(`Example app listening on port ${port}!`))
+
+ //회원 가입할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+app.post('/register', async (req, res) =>{
+    const user = new User(req.body)
+    await user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.json({
+        success: false,
+        err: err,
+      });
+    });
+});
+
+// app.post('/register',(req, res)=>{
+//     //회원가입할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+//     const user = new User(req.body)
+
+//     user.save((err, userInfo) => {
+//         if(err) return res.json({ success: false, err})
+//         return res.status(200).json({
+//             success:true
+//         })
+//     })
+// })
